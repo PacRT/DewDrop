d3f1612a70282e238207abe054258362cbe4288c
Chiradip Mandal <chiradip@chiradip.com>
2016-03-24 20:16:59 -0700

    authscheme bug fixed

--- mqtt-server.js
+++ mqtt-server.js
@@ -1,6 +1,6 @@
 var tls       = require('tls');
 //var fs        = require('fs');
-var aedes     = require('aedes')();
+var mqstream     = require('mqstream')();
 var http      = require('http');
 var https     = require('https');
 var net       = require('net');
@@ -16,7 +16,7 @@ var server; // global variable to have a clean EXIT
 
 if(config.has('mqtts')) {
   var options = config.mqtts;
-  var mqtts_server = tls.createServer(options, aedes.handle);
+  var mqtts_server = tls.createServer(options, mqstream.handle);
   mqtts_server.listen(config.mqtts.port, function() {
     log.info('Secure DewDropQ is bound on port ', config.mqtts.port);
   });
@@ -24,7 +24,7 @@ if(config.has('mqtts')) {
 
 if(config.has('mqtt')) {
   var options = config.mqtt;
-  var mqtt_server = net.createServer({}, aedes.handle);
+  var mqtt_server = net.createServer({}, mqstream.handle);
   server = mqtt_server;
   mqtt_server.listen(config.mqtt.port, function() {
     log.info('DewDropQ (non-TLS) is bound on port ', config.mqtt.port);
@@ -34,7 +34,7 @@ if(config.has('mqtt')) {
 if(config.has('ws')) {
   var http_server = http.createServer();
   server = http_server;
-  websocket.createServer({server: http_server}, aedes.handle);
+  websocket.createServer({server: http_server}, mqstream.handle);
   http_server.listen(config.ws.port, config.ws.host, function() {
     log.info('DewDropQ-WS is bound on port ', config.ws.port);
   });
@@ -44,47 +44,47 @@ if(config.has('wss')) {
   var options = config.wss;
   var https_server = https.createServer(options);
   server = https_server;
-  websocket.createServer({server: https_server}, aedes.handle);
+  websocket.createServer({server: https_server}, mqstream.handle);
   https_server.listen(config.wss.port, config.wss.host, function() {
     log.info('DewDropQ-WSS is bound on port ', config.wss.port);
   });
 }
 
-aedes.on('client', function(client) {
+mqstream.on('client', function(client) {
   log.info('Client: ', client.id, ' connected..');
   if(client.id != null) ++numClient;
   log.info('Connected clients: ', numClient);
-  aedes.publish({topic: 'clientcount', payload: new Buffer(numClient.toString(), 'utf8'), retain: true}, function() {})
+  mqstream.publish({topic: 'clientcount', payload: new Buffer(numClient.toString(), 'utf8'), retain: true}, function() {})
 });
 
-aedes.on('clientDisconnect', function(client) {
+mqstream.on('clientDisconnect', function(client) {
   log.info('Client: ', client.id, ' disconnected..');
   if(client.id != null) --numClient;
   log.info('Connected clients: ', numClient);
-  aedes.publish({topic: 'clientcount', payload: new Buffer(numClient.toString(), 'utf8'), retain: true}, function() {})
+  mqstream.publish({topic: 'clientcount', payload: new Buffer(numClient.toString(), 'utf8'), retain: true}, function() {})
 });
 
-aedes.on('clientError', function(client, error) {
+mqstream.on('clientError', function(client, error) {
   log.info('Client: ', client.id, ' has error: ', error);
 });
 
-aedes.on('publish', function(packet, client) {
+mqstream.on('publish', function(packet, client) {
   var clientid;
   if(client == null) clientid = null;
   log.info('Client: ', clientid, ' published packet: ', packet);
 });
 
-aedes.on('subscribe', function(subscriptions, client) {
+mqstream.on('subscribe', function(subscriptions, client) {
   log.info('Client: ', client.id, ' subscribed: ', subscriptions);
 });
 
-aedes.on('unsubscribe', function(unsubscriptions, client) {
+mqstream.on('unsubscribe', function(unsubscriptions, client) {
   log.info('Client: ', client.id, ' unsubscribed: ', unsubscriptions);
 });
 
-aedes.authenticate = function (client, username, password, callback) {
+mqstream.authenticate = function (client, username, password, callback) {
 
-  if(config.auth_scheme.clientcert === true) {
+  if(config.auth_scheme != null && config.auth_scheme.clientcert === true) {
     var subj = client.conn.getPeerCertificate().subject;
     log.info('Cert Subject: ', subj);
     return callback(null, true)
@@ -94,7 +94,7 @@ aedes.authenticate = function (client, username, password, callback) {
   callback(null, true);
 }
 
-aedes.authorizePublish = function (client, packet, callback) {
+mqstream.authorizePublish = function (client, packet, callback) {
   log.info('AuthorizePublish is called')
   if (packet.topic === 'aaaa') {
     return callback(new Error('wrong topic'))
@@ -106,9 +106,9 @@ aedes.authorizePublish = function (client, packet, callback) {
   callback(null)
 }
 
-aedes.authorizeSubscribe = function (client, sub, callback) {
+mqstream.authorizeSubscribe = function (client, sub, callback) {
   if (sub.topic === 'aaaa') {
-    return cb(new Error('wrong topic'))
+    return callback(new Error('wrong topic'))
   }
   if (sub.topic === 'bbb') {
     // overwrites subscription
