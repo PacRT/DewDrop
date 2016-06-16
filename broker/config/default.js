3e5f36644d8fcd3c6bb1a39aadff360aac2bf610
Chiradip Mandal <chiradip@chiradip.com>
2016-06-15 20:07:27 -0700

    cypher syntax corrected

--- broker/config/default.js
+++ broker/config/default.js
@@ -105,7 +105,7 @@ module.exports =
             .run("match (n " + neoobject + ")-[:PUB]-> ({name: '" + topic + "'}) return count(n)")
             .then(function(result){
                 log.info("Result: ", result)
-                log.info("result.records[0]: ", result.records[0])
+                log.info("result.records: ", result.records)
                 //var count = 0
                 if(result.records.length > 0) {
                     callback(null) // callback with no error
